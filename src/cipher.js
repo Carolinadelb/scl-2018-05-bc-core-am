f31b54f69120b6f9e39f3720d55a7587e72a1bcb
Carolinadelb <carolinadelgadob@gmail.com>
2018-05-31 15:11:41 -0400

    Con ambas funciones FUNCIONANDO encode y decode

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,50 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (text, shift) => {
+    let textCipher = "";
+    let tUpper;   
+    shift = parseInt(shift); 
+    //formula de iteración i
+    for (let i= 0; i<text.length;i++){
+     //volver texto a mayúscula
+      let tUpper = text.toUpperCase();
+     // Formula Michelle
+      let finalT = (tUpper.charCodeAt(i) - 65 + parseInt(shift)) % 26 + 65; 
+     //Para espacio entre palabras
+      if (finalT === 32){
+        textCipher += " ";
+        } else{ 
+      //vuelvo a transformar a string el caracter numerico desplazado  
+        textCipher+= String.fromCharCode(finalT);
+       }   
+    }
+    //Respuesta final:
+    return "Tu Cifrado es: " + textCipher
+
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
-}
+
+  decode: (text2, shiftD) => {
+    let textDecipher = "";
+    shiftD = parseInt(shiftD);
+    //Iteración
+    for (let i= 0; i<text2.length; i++){
+      //volver texto en mayúsculas
+      let tUpperD = text2.toUpperCase();
+      let finalTD = (tUpperD.charCodeAt(i));
+      //Para los espacios 32(es espacio) así que mostarra espacio
+      if (finalTD === 32){
+        // no sobrescribe, se suman 
+        textDecipher += " ";
+        } else { 
+        let resultD = (finalTD - 65 - parseInt(shiftD)) % 26 + 65;
+        if (resultD < 65) {
+          resultD +=26;
+        }
+        let showD = String.fromCharCode(resultD);
+        textDecipher += showD;
+      }
+        }
+        return "Tu Decifrado es: " + textDecipher;
+    }
+   }
+  
+
